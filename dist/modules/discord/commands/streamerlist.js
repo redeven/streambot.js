d6556c83a451e2fd50583f9543a2bbd71fe35e35
Redeven <18680184+redeven@users.noreply.github.com>
2022-06-05 17:55:57 -0300

    Only show enabled sources in streamerlist command

--- dist/modules/discord/commands/streamerlist.js
+++ dist/modules/discord/commands/streamerlist.js
@@ -3,13 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.STREAMER_LIST_FACTORY = void 0;
 const builders_1 = require("@discordjs/builders");
 const utils_1 = require("../../../shared/utils/utils");
-const STREAMER_LIST_FACTORY = (configService) => {
+const STREAMER_LIST_FACTORY = (configService, sources) => {
     return {
         data: new builders_1.SlashCommandBuilder().setName('streamerlist').setDescription('Show list of streamers'),
         execute: (interaction) => {
             var _a;
             if ((_a = interaction.guild) === null || _a === void 0 ? void 0 : _a.id) {
-                const SOURCES = Object.entries(configService.getConfiguration().guilds[interaction.guild.id].sources).map(([name, source]) => `${(0, utils_1.capitalize)(name)}: ${Object.values(source)
+                const SOURCES = Object.entries(configService.getConfiguration().guilds[interaction.guild.id].sources)
+                    .filter(([name, source]) => Object.keys(sources).includes(name))
+                    .map(([name, source]) => `${(0, utils_1.capitalize)(name)}: ${Object.values(source)
                     .map((streamer) => (0, utils_1.sanitize)(streamer.displayName))
                     .join(', ')}`);
                 interaction.reply({ content: `Channels:\n${SOURCES.join('\n')}`, ephemeral: true });
--- src/modules/discord/commands/streamerlist.ts
+++ src/modules/discord/commands/streamerlist.ts
@@ -3,17 +3,19 @@ import { CommandInteraction } from 'discord.js';
 import { CommandFactory } from '../../../shared/interfaces/discord';
 import { capitalize, sanitize } from '../../../shared/utils/utils';
 
-export const STREAMER_LIST_FACTORY: CommandFactory = (configService) => {
+export const STREAMER_LIST_FACTORY: CommandFactory = (configService, sources) => {
   return {
     data: new SlashCommandBuilder().setName('streamerlist').setDescription('Show list of streamers'),
     execute: (interaction: CommandInteraction) => {
       if (interaction.guild?.id) {
-        const SOURCES = Object.entries(configService.getConfiguration().guilds[interaction.guild.id].sources).map(
-          ([name, source]) =>
-            `${capitalize(name)}: ${Object.values(source)
-              .map((streamer) => sanitize(streamer.displayName))
-              .join(', ')}`,
-        );
+        const SOURCES = Object.entries(configService.getConfiguration().guilds[interaction.guild.id].sources)
+          .filter(([name, source]) => Object.keys(sources).includes(name))
+          .map(
+            ([name, source]) =>
+              `${capitalize(name)}: ${Object.values(source)
+                .map((streamer) => sanitize(streamer.displayName))
+                .join(', ')}`,
+          );
         interaction.reply({ content: `Channels:\n${SOURCES.join('\n')}`, ephemeral: true });
       }
     },

e0141b4672fb934602afe89888700526f29953bf
Redeven <18680184+redeven@users.noreply.github.com>
2022-06-05 09:18:57 -0300

    Remove useless interface opts

--- dist/shared/interfaces/streambot.model.d.ts
+++ dist/shared/interfaces/streambot.model.d.ts
@@ -1,12 +1,6 @@
 import { SJSDiscordInitOpts } from './discord';
-import { TrovoSourceOpts } from './trovo.source.model';
-import { TwitchSourceOpts } from './twitch.source.model';
 export interface StreambotJsInitOpts {
     discordOpts: SJSDiscordInitOpts;
-    sources: {
-        twitch: TwitchSourceOpts;
-        trovo: TrovoSourceOpts;
-    };
 }
 export interface StreambotJsSslCert {
     key: string;
